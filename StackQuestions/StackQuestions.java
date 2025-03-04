aa36a4dbeab49547d45dacc3af32fc7c523008ae
KundanPW <kk9797307@gmail.com>
2025-03-05 03:04:19 +0530

    Added Q2. Reverse of a string using stack

--- StackQuestions/StackQuestions.java
+++ StackQuestions/StackQuestions.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class StackQuestions {
-    public static void pushAtBottom(Stack<Integer> s, int data) {
+    public static void pushAtBottom(Stack<Integer> s, int data) {   // Q1. Push at the bottom of the stack
         if(s.isEmpty()) {
             s.push(data);
             return;
@@ -12,15 +12,25 @@ public class StackQuestions {
         s.push(top);
     }
 
-    public static void main(String args[]) {
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-
-        pushAtBottom(s, 4);
-        while(!s.isEmpty()) {
-            System.out.println(s.pop());
+    public static String reverseString(String str) {  // Q2. Reverse a string using a stack
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while(idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
         }
+
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()) {
+            char curr=s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+
+    public static void main(String args[]) {
+       String str = "abc";
+       String result = reverseString(str);
+       System.out.println(result);
     }
 }
