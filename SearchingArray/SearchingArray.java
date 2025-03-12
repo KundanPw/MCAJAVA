d76eaea6d290e97f0d59779468d69a83be452055
KundanPW <kk9797307@gmail.com>
2025-03-12 16:21:29 +0530

    Added Binary search using array

--- SearchingArray/SearchingArray.java
+++ SearchingArray/SearchingArray.java
@@ -9,11 +9,33 @@
         return-1;
     }
 
+    public static int binarySearch(int num[], int key) {
+        int start = 0;
+        int end = num.length-1;
+        while (start <= end) {
+            int mid = (start+end)/2;
+            if(num[mid]==key) {
+                return mid;
+            }
+
+            // right part
+            if(num[mid] < key) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
 
         int number[] = {4, 8, 6, 11, 14, 16};
         int key = 11;
-        int idx = linearSearch(number, key);
+
+        // int idx = linearSearch(number, key);
+        int idx = binarySearch(number, key);
+
         if(idx==-1) {
            System.out.println("Not found");
         } else {
