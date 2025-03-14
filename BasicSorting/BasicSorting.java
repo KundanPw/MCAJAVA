bc436ae8066d5aed649132ea21d71efb435fc44d
KundanPW <kk9797307@gmail.com>
2025-03-14 23:04:28 +0530

    Added Selection sort problem

--- BasicSorting/BasicSorting.java
+++ BasicSorting/BasicSorting.java
@@ -14,6 +14,21 @@ public class BasicSorting {
         }
     }
 
+    public static void SelectionSort(int arr[]) {
+        for(int i=0; i<arr.length-1; i++) {
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            // swapping 
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
     public static void printArray(int arr[]) {
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i]+" ");
@@ -23,7 +38,9 @@ public class BasicSorting {
 
     public static void main(String[] args) {
         int arr[] = {3, 9, 6, 5, 2, 8};
-        BubbleSort(arr);
+        // BubbleSort(arr);
+
+        SelectionSort(arr);
         printArray(arr);
     }
 }
