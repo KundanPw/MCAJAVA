d49e1054af801927534369104c359a0c9828acd7
KundanPW <kk9797307@gmail.com>
2025-03-14 23:23:59 +0530

    Added insertion sort problem

--- BasicSorting/BasicSorting.java
+++ BasicSorting/BasicSorting.java
@@ -29,6 +29,19 @@ public class BasicSorting {
         }
     }
 
+    public static void insertionSort(int arr[]) {
+        for(int i=1; i<arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >= 0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            // insertion
+            arr[prev+1] = curr;
+        }
+    }
+
     public static void printArray(int arr[]) {
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i]+" ");
@@ -38,9 +51,10 @@ public class BasicSorting {
 
     public static void main(String[] args) {
         int arr[] = {3, 9, 6, 5, 2, 8};
-        // BubbleSort(arr);
 
-        SelectionSort(arr);
+        // BubbleSort(arr);
+        // SelectionSort(arr);
+        insertionSort(arr);
         printArray(arr);
     }
 }
