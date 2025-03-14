4bffef5070b56896585e4c3025ab58517d619c72
KundanPW <kk9797307@gmail.com>
2025-03-14 16:30:02 +0530

    Added Assignement Q1 Approach2

--- ArrayProblemPart2/ArrayProblemPart2.java
+++ ArrayProblemPart2/ArrayProblemPart2.java
@@ -1,11 +1,32 @@
+import java.util.HashSet;
 
 public class ArrayProblemPart2 {
-    public static boolean containDuplicate(int number[]) {
+    /**
+     * Given an integer array nums, return true if any value appears at least twice in the array,
+     * and return flase if every element is distinct.
+     */
+
+    /* 
+    public static boolean containDuplicate(int number[]) {  // Approach1-Using set TC: O(n2)
         for(int i=0; i<number.length-1; i++) {
             for(int j=i+1; j<number.length; j++) {
                 if(number[i]==number[j]) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    */
+
+    public static boolean containDuplicate(int number[]) {   // Approach2-Using set TC: O(n)
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<number.length; i++) {
+            if(set.contains(number[i])) {
+                return true;
+            } else {
+                set.add(number[i]);
             }
         }
         return false;
