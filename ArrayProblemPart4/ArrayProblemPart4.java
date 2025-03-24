b5dc54dec1e60deeebb5f2b8bf462d160f39c63b
KundanPW <kk9797307@gmail.com>
2025-03-24 14:47:24 +0530

    Added Q2. Findout the second largest element in the given array

--- ArrayProblemPart4/ArrayProblemPart4.java
+++ ArrayProblemPart4/ArrayProblemPart4.java
@@ -9,13 +9,29 @@ public class ArrayProblemPart4 {
         return res;
     }
 
+    public static int secondLargestElement(int arr[]) {  // Q2. Findout the second largest element in the given array
+        int largest = -1, secondLargest = -1;
+        for(int i=0; i<arr.length; i++) {
+            if(largest < arr[i]) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if(arr[i] < largest && arr[i] > secondLargest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
     
 
     public static void main(String[] ars) {
-        int arr[] = {10, 20, 30, 40, 50};
-        ArrayList<Integer> res = alternatePrintArray(arr);
-        for(int a: res) {
-            System.out.print(a+" ");
-        }
+        int arr[] = {10, 70, 89, 40, 5};
+
+        // ArrayList<Integer> res = alternatePrintArray(arr);
+        // for(int a: res) {
+        //     System.out.print(a+" ");
+        // }
+
+        System.out.println(secondLargestElement(arr));
+
     }
 }
