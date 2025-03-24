1b063b01580b304ec130b26e5c3d84daf5daced6
KundanPW <kk9797307@gmail.com>
2025-03-24 15:51:08 +0530

    Added Q4. find leader element of given array

--- ArrayProblemPart4/ArrayProblemPart4.java
+++ ArrayProblemPart4/ArrayProblemPart4.java
@@ -49,10 +49,30 @@ public class ArrayProblemPart4 {
 
         return res;
     }
+
+    // Q4. Given an array arr[] of size n, the task is to find all the leaders in the array. An element is a leader if it is greater than or equal to all the elements to its right side: Exm: arr[] = [16, 17, 4, 3, 5, 2] and output = [17, 5, 2]
+
+    static ArrayList<Integer> leaderElement(int arr[]) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int n = arr.length;
+        for(int i=0; i<n; i++) {
+            int j;
+            for(j=i+1; j<n; j++) {
+                if(arr[i] < arr[j]) { // find largest element
+                    break;
+                }
+            }
+
+            if(j==n) { // no found largest element
+                res.add(arr[i]);
+            }
+        }
+        return res;
+    }
     
 
     public static void main(String[] ars) {
-        int arr[] = {10, 70, 89, 40, 5};
+        //int arr[] = {10, 70, 89, 40, 5};
 
         // ArrayList<Integer> res = alternatePrintArray(arr);
         // for(int a: res) {
@@ -61,8 +81,15 @@ public class ArrayProblemPart4 {
 
         // System.out.println(secondLargestElement(arr));
 
-        List<Integer> res = getThreeLargestElement(arr);
-        for(int a: res) {
+        // List<Integer> res = getThreeLargestElement(arr);
+        // for(int a: res) {
+        //     System.out.print(a+" ");
+        // }
+
+        int arr[] = {16, 17, 4, 3, 5, 2};
+
+        ArrayList<Integer> result = leaderElement(arr);
+        for(int a:result) {
             System.out.print(a+" ");
         }
     }
